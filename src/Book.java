1806c815c8b6e75d63c48f3360b4dd446c3dacd5
AdamKhales <adamkhales2006@gmail.com>
2024-12-07 23:37:04 -0500

    book class and library class

--- src/Book.java
+++ src/Book.java
@@ -61,6 +61,39 @@ public class Book {
             return true;
         }else return false;
     }
+    public Book copy() {
+        return new Book(title, author, price, publisher, isbn);
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public String getPublisher() {
+        return publisher;
+    }
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+    public String getIsbn() {
+        return isbn;
+    }
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
 }
 
 
---
+++ src/Library.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Library {
+    ArrayList<Book> books = new ArrayList<>();
+    public Library() {
+        books = new ArrayList<>(5);
+    }
+    public Library(ArrayList<Book> books) {
+        this.books = books;
+    }
+    public Library(Library library) {
+        this.books = new ArrayList<>();
+        for (Book book : library.books) {
+            this.books.add(new Book(book));
+        }
+    }
+    public ArrayList<Book> searchBook(String search) {
+        ArrayList<Book> seachName = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getTitle().toLowerCase().contains(search.toLowerCase())) {
+                seachName.add(book);
+            }
+        }
+        return seachName;
+    }
+    public boolean equals(Library library) {
+        if (this == library){
+            return true;
+        }else return false;
+    }
+    public String toString() {
+        String string = "Library:\n";
+        for (Book book : books) {
+            string += book.toString() + "\n";
+        }
+        return string;
+    }
+
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+    public void setBooks(ArrayList<Book> books) {
+        this.books = books;
+    }
+}
